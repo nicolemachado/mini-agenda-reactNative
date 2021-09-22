d65f45ed186baa9baa6a744208f2e6b0fafdbd33
nicole machado <nicole.ilimitada@gmail.com>
2021-09-21 23:48:06 -0300

    mini agenda

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Alert} from 'react-native';
+
+
 
 export default function App() {
+
+    const [text, onChangeText] = React.useState("");
+    const [tel, onChangeTel] = React.useState(null);
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{fontWeight: 'bold'}}
+      >Mini Agenda-Telefônica</Text>
+      <TextInput
+       style={styles.input}
+       placeholder="Digite o nome que deseja salvar"
+       onChangeText={onChangeText}
+       value={text}
+      />
+      <TextInput
+       style={styles.input}
+       onChangeText={onChangeTel}
+       value={tel}
+       placeholder="Por favor, digite o número"
+       keyboardType="numeric"
+      />
       <StatusBar style="auto" />
+
+      <Button 
+      color="#f194ff"
+      title="SALVAR"
+      onPress={() => Alert.alert(`"Você acabou de salvar o contato de ${text}! :)`)}
+      />
     </View>
   );
 }
@@ -14,8 +41,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FDF4FC',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  input: {
+    backgroundColor: '#fff',
+    width: 300,
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius:10,
+  },
+
 });
